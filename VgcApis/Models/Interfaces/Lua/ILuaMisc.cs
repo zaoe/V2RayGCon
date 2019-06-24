07cd0789c061c37cc09a97b9fa2283d5444f7dbb
nobody <nobody@nowhere.mars>
2019-06-24 10:50:55 +0800

    Bump version to 1.2.0.0.

--- VgcApis/Models/Interfaces/Lua/ILuaMisc.cs
+++ VgcApis/Models/Interfaces/Lua/ILuaMisc.cs
@@ -2,12 +2,20 @@
 {
     public interface ILuaMisc
     {
+        // share among all scripts
         string ReadLocalStorage(string key);
+
+        // share among all scripts
         void WriteLocalStorage(string key, string value);
 
         #region encode decode
+        // v2cfg://(b64Str)
         string AddV2cfgPrefix(string b64Str);
+
+        // v://(b64Str)
         string AddVeePrefix(string b64Str);
+
+        // vmess://(b64Str)
         string AddVmessPrefix(string b64Str);
 
         string Base64Encode(string text);
@@ -18,11 +26,16 @@
         string Config2VmessLink(string config);
 
         string ShareLink2ConfigString(string shareLink);
+
+        // links = "vmess://... ss://...  (...)"
         int ImportLinks(string links, string mark);
         #endregion
 
         string GetAppDir();
+
+        // GetLinkBody("vmess://abcdefg") == "abcdefg"
         string GetLinkBody(string link);
+
         string PredefinedFunctions();
         void Print(params object[] contents);
         string ScanQrcode();
--- VgcApis/Models/Interfaces/Lua/ILuaServer.cs
+++ VgcApis/Models/Interfaces/Lua/ILuaServer.cs
@@ -7,12 +7,20 @@ namespace VgcApis.Models.Interfaces.Lua
         List<ICoreServCtrl> GetAllServers();
         string PackSelectedServers(string orgUid, string pkgName);
         void RequireFormMainReload();
+
+        // wont refresh form main
         void ResetIndexQuiet();
+
+        // download (testUrl)
         long RunCustomSpeedTest(string rawConfig, string testUrl, int testTimeout);
+
+        // download google.com
         long RunSpeedTest(string rawConfig);
         bool RunSpeedTestOnSelectedServers();
         void SortSelectedServersBySpeedTest();
         void SortSelectedServersBySummary();
+
+        // refresh servers' title in form main
         void UpdateAllSummary();
     }
 }
